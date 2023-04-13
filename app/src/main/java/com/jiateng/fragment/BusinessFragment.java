9db26b17b451de4ccacfb28b0f49149aff2aa951
自己 <zzq6693117@163.com>
2023-04-13 21:36:19 +0800

    商家 展示

--- app/src/main/java/com/jiateng/fragment/BusinessFragment.java
+++ app/src/main/java/com/jiateng/fragment/BusinessFragment.java
@@ -4,9 +4,12 @@ import android.content.Intent;
 import android.net.Uri;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.jiateng.R;
+import com.jiateng.bean.School;
 import com.jiateng.common.base.BaseFragment;
+import com.jiateng.common.utils.SharedPreferencesUtil;
 import com.lidroid.xutils.ViewUtils;
 import com.lidroid.xutils.view.annotation.ViewInject;
 
@@ -20,7 +23,20 @@ import com.lidroid.xutils.view.annotation.ViewInject;
 public class BusinessFragment extends BaseFragment {
     @ViewInject(R.id.shop_business_phone)
     private ImageView phone;
+    @ViewInject(R.id.shop_business_name)
+    private TextView shopBusinessName;
+    @ViewInject(R.id.shop_business_time)
+    private TextView shopBusinessTime;
+    @ViewInject(R.id.shop_business_local)
+    private TextView shopBusinessLocal;
 
+    private School school;
+
+    public BusinessFragment(School shopInfo) {
+        super();
+        this.school = shopInfo;
+
+    }
     @Override
     protected View initView() {
         View view = View.inflate(context, R.layout.fragment_business, null);
@@ -32,12 +48,21 @@ public class BusinessFragment extends BaseFragment {
     private void init() {
         phone.setOnClickListener(v -> {
             //TODO 设置电话号获取上个界面传过来的值
-            String phoneNo = "12345";
+            String phoneNo = school.getSchoolIphone();
             Intent intent = new Intent();
             intent.setAction(Intent.ACTION_DIAL);
             Uri uri = Uri.parse("tel:" + phoneNo);
             intent.setData(uri);
             startActivity(intent);
         });
+    }
+
+
+    @Override
+    protected void initData() {
+        super.initData();
+        shopBusinessName.setText(school.getSchoolName());
+        shopBusinessTime.setText(school.getSchoolContent());
+        shopBusinessLocal.setText(school.getSchoolAddress());
     }
 }
